7003009d8182b27d351724333885de5974c55d1e
dinilkumar <dinilkumar.s@gmail.com>
2021-07-14 20:18:16 +0530

    admin landing page

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { StudentRegisterComponent } from './student-register/student-register.component';
 import { LoginComponent } from './login/login.component';
 import { NavHeaderComponent } from './nav-header/nav-header.component';
+import { AdminComponent } from './admin/admin.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path:'register',component: StudentRegisterComponent },
-  { path:'login',component:LoginComponent }
+  { path:'login',component:LoginComponent },
+  { path: 'admin',component:AdminComponent}
 ];
 
 @NgModule({
